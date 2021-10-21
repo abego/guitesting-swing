59d2c0ef58cc2d75f9b2bd72406e4aed547d78ab
Udo Borkowski <ub@abego-software.de>
2021-10-22 00:28:26 +0200

    int: extract ExpectedActualDifferenceImageViewerWidget

---
+++ abego-guitesting-swing/src/main/java/org/abego/guitesting/swing/internal/snapshotreview/ExpectedActualDifferenceImageViewerWidget.java
@@ -0,0 +1,177 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Udo Borkowski, (ub@abego.org)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.abego.guitesting.swing.internal.snapshotreview;
+
+import org.abego.guitesting.swing.ScreenCaptureSupport;
+import org.abego.guitesting.swing.internal.util.SwingUtil;
+import org.eclipse.jdt.annotation.Nullable;
+
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.util.Objects;
+
+import static java.lang.Math.max;
+import static javax.swing.SwingUtilities.invokeLater;
+import static org.abego.guitesting.swing.internal.util.SwingUtil.flowLeft;
+import static org.abego.guitesting.swing.internal.util.SwingUtil.onComponentResized;
+
+class ExpectedActualDifferenceImageViewerWidget implements Widget {
+    private static final Color EXPECTED_BORDER_COLOR = new Color(0x59A869);
+    private static final Color ACTUAL_BORDER_COLOR = new Color(0xC64D3F);
+    private static final Color DIFFERENCE_BORDER_COLOR = new Color(0x6E6E6E);
+    private static final int BORDER_SIZE = 3;
+
+    private final JLabel[] labelsForImages =
+            new JLabel[]{new JLabel(), new JLabel(), new JLabel()};
+    private final JComponent imagesContainer = flowLeft(c -> {
+        c.setOpaque(true);
+        c.setBackground(Color.white);
+        c.setBorder(null);
+    }, labelsForImages);
+    @Nullable
+    private SnapshotImages snapshotImages;
+    private boolean shrinkToFit = true;
+    private int expectedImageIndex;
+    private ScreenCaptureSupport.@Nullable SnapshotIssue snapshotIssue;
+
+    public static ExpectedActualDifferenceImageViewerWidget newExpectedActualDifferenceWidget() {
+        return new ExpectedActualDifferenceImageViewerWidget();
+    }
+
+    public ScreenCaptureSupport.@Nullable SnapshotIssue getSnapshotIssue() {
+        return snapshotIssue;
+    }
+
+    public void setSnapshotIssue(ScreenCaptureSupport.@Nullable SnapshotIssue snapshotIssue) {
+        this.snapshotIssue = snapshotIssue;
+        updateLabelsForImages();
+        updateImagesContainer();
+    }
+
+    public boolean getShrinkToFit() {
+        return shrinkToFit;
+    }
+
+    public void setShrinkToFit(boolean shrinkToFit) {
+        this.shrinkToFit = shrinkToFit;
+        updateLabelsForImages();
+    }
+
+    public int getExpectedImageIndex() {
+        return expectedImageIndex;
+    }
+
+    public void setExpectedImageIndex(int expectedImageIndex) {
+        this.expectedImageIndex = expectedImageIndex;
+        updateLabelsForImages();
+    }
+
+    public Color getExpectedBorderColor() {return EXPECTED_BORDER_COLOR;}
+
+    public Color getActualBorderColor() {return ACTUAL_BORDER_COLOR;}
+
+    public Color getDifferenceBorderColor() {return DIFFERENCE_BORDER_COLOR;}
+
+    @Override
+    public JComponent getComponent() {
+        return imagesContainer;
+    }
+
+    private ExpectedActualDifferenceImageViewerWidget() {
+        onComponentResized(imagesContainer, e -> onImagesContainerVisibleRectChanged());
+    }
+
+    private void onImagesContainerVisibleRectChanged() {
+        if (getShrinkToFit()) {
+            invokeLater(this::updateLabelsForImages);
+        }
+    }
+
+    private void updateLabelsForImages() {
+        @Nullable SnapshotImages images = getSnapshotImages();
+        if (images != null) {
+            setIconAndLinedBorder(
+                    labelsForImages[(expectedImageIndex) % 3],
+                    images.getExpectedImage(),
+                    getExpectedBorderColor());
+            setIconAndLinedBorder(
+                    labelsForImages[(expectedImageIndex + 1) % 3],
+                    images.getActualImage(),
+                    getActualBorderColor());
+            setIconAndLinedBorder(
+                    labelsForImages[(expectedImageIndex + 2) % 3],
+                    images.getDifferenceImage(),
+                    getDifferenceBorderColor());
+
+            onLabelsForImagesChanged();
+        }
+    }
+
+    private void updateImagesContainer() {
+        SwingUtil.setVisible(getSnapshotIssue() != null, labelsForImages);
+    }
+
+    private void setIconAndLinedBorder(JLabel label, ImageIcon icon, Color borderColor) {
+        label.setIcon(icon);
+        label.setBorder(SwingUtil.lineBorder(borderColor, BORDER_SIZE));
+    }
+
+    @Nullable
+    private Dimension getImagesArea() {
+        if (getShrinkToFit()) {
+            Rectangle visibleRect = imagesContainer.getVisibleRect();
+            int w = visibleRect.width - 4 * SwingUtil.DEFAULT_FLOW_GAP - 6 * BORDER_SIZE;
+            int h = visibleRect.height - 2 * SwingUtil.DEFAULT_FLOW_GAP - 2 * BORDER_SIZE;
+            return new Dimension(max(0, w), max(0, h));
+        } else {
+            return null;
+        }
+    }
+
+    private void onLabelsForImagesChanged() {
+        invokeLater(this::updateImagesContainer);
+    }
+
+    @Nullable
+    private SnapshotImages getSnapshotImages() {
+        ScreenCaptureSupport.@Nullable SnapshotIssue issue = getSnapshotIssue();
+        if (issue == null) {
+            return null;
+        }
+        @Nullable SnapshotImages images = snapshotImages;
+        if (images == null
+                || !Objects.equals(images.getIssue(), issue)
+                || !Objects.equals(getImagesArea(), images.getArea())) {
+            images = new SnapshotImages(issue, getImagesArea());
+            snapshotImages = images;
+        }
+        return images;
+    }
+
+}
--- abego-guitesting-swing/src/main/java/org/abego/guitesting/swing/internal/snapshotreview/SnapshotReviewPane.java
+++ abego-guitesting-swing/src/main/java/org/abego/guitesting/swing/internal/snapshotreview/SnapshotReviewPane.java
@@ -36,7 +36,6 @@ import org.eclipse.jdt.annotation.Nullable;
 
 import javax.swing.Action;
 import javax.swing.DefaultListModel;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
@@ -47,13 +46,11 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.Rectangle;
-import java.util.Objects;
 
-import static java.lang.Math.max;
 import static javax.swing.BorderFactory.createLineBorder;
 import static javax.swing.SwingUtilities.invokeLater;
 import static org.abego.commons.io.FileUtil.toFile;
+import static org.abego.guitesting.swing.internal.snapshotreview.ExpectedActualDifferenceImageViewerWidget.newExpectedActualDifferenceWidget;
 import static org.abego.guitesting.swing.internal.util.FileUtil.copyFile;
 import static org.abego.guitesting.swing.internal.util.SwingUtil.DEFAULT_FLOW_GAP;
 import static org.abego.guitesting.swing.internal.util.SwingUtil.bordered;
@@ -65,7 +62,6 @@ import static org.abego.guitesting.swing.internal.util.SwingUtil.label;
 import static org.abego.guitesting.swing.internal.util.SwingUtil.newAction;
 import static org.abego.guitesting.swing.internal.util.SwingUtil.newDefaultListModel;
 import static org.abego.guitesting.swing.internal.util.SwingUtil.newListCellRenderer;
-import static org.abego.guitesting.swing.internal.util.SwingUtil.onComponentResized;
 import static org.abego.guitesting.swing.internal.util.SwingUtil.scrollingNoBorder;
 import static org.abego.guitesting.swing.internal.util.SwingUtil.separatorBar;
 import static org.abego.guitesting.swing.internal.util.SwingUtil.vlist;
@@ -74,15 +70,17 @@ import static org.abego.guitesting.swing.internal.util.UpdateableSwingUtil.check
 //TODO: better split between init, style (e.g. border), layout, binding/updating
 class SnapshotReviewPane<T extends SnapshotIssue> extends JPanel {
 
-    private static final Color EXPECTED_BORDER_COLOR = new Color(0x59A869);
-    private static final Color ACTUAL_BORDER_COLOR = new Color(0xC64D3F);
-    private static final Color DIFFERENCE_BORDER_COLOR = new Color(0x6E6E6E);
-    private static final Color TITLE_BAR_COLOR = new Color(0xE2E6Ec);
-    private static final int BORDER_SIZE = 3;
-    private static final int LEGEND_BORDER_SIZE = 2;
-    private static final int VISIBLE_ISSUES_COUNT = 8;
-    private static final int BULLET_SIZE = 24;
-    private static final Font BULLET_FONT = new Font(Font.SANS_SERIF, Font.PLAIN, BULLET_SIZE);
+
+    /**
+     * Defines the visual appearance of individual components, how they look like.
+     */
+    static class Style {
+        private static final Color TITLE_BAR_COLOR = new Color(0xE2E6Ec);
+        private static final int LEGEND_BORDER_SIZE = 2;
+        private static final int VISIBLE_ISSUES_COUNT = 8;
+        private static final int BULLET_SIZE = 24;
+        private static final Font BULLET_FONT = new Font(Font.SANS_SERIF, Font.PLAIN, BULLET_SIZE);
+    }
 
     // Actions
     private final Action addAltenativeSnapshotAction;
@@ -96,11 +94,10 @@ class SnapshotReviewPane<T extends SnapshotIssue> extends JPanel {
     private final Action toggleShrinkToFitAction;
 
     // Components (for more see #layoutComponent)
-    private final JLabel[] labelsForImages;
     private final JLabel[] labelsForLegend;
     private final JLabel selectedIssueDescriptionLabel;
     private final JComponent imagesLegendContainer;
-    private final JComponent imagesContainer;
+    private final ExpectedActualDifferenceImageViewerWidget expectedActualDifferenceImageViewerWidget;
     private final JComponent variantsIndicator;
     private final JButton ignoreButton;
     private final JCheckBoxUpdateable shrinkToFitCheckBox;
@@ -109,17 +106,11 @@ class SnapshotReviewPane<T extends SnapshotIssue> extends JPanel {
     // UI State
     private final DefaultListModel<T> issuesListModel;
 
-    // State
-    private int expectedImageIndex;
-    private boolean shrinkToFit;
-    @Nullable
-    private SnapshotImages snapshotImages;
 
     SnapshotReviewPane(Seq<T> issues) {
         // init State
         issuesListModel = newDefaultListModel(
                 issues.sortedBy(SnapshotIssue::getLabel));
-        shrinkToFit = true;
 
         // init Actions
         addAltenativeSnapshotAction = newAction("Make Actual an Alternative (A)", KeyStroke.getKeyStroke("A"), Icons.alternativeIcon(), e -> addAltenativeSnapshot()); //NON-NLS
@@ -130,25 +121,20 @@ class SnapshotReviewPane<T extends SnapshotIssue> extends JPanel {
         rotateImageAction = newAction("Rotate Images (→)", KeyStroke.getKeyStroke("RIGHT"), Icons.rotateRightIcon(), e -> rotateImages()); //NON-NLS;
         toggleShrinkToFitAction = newAction("Shrink to Fit (#)", KeyStroke.getKeyStroke("NUMBER_SIGN"), e -> toggleShrinkToFit()); //NON-NLS
 
+        expectedActualDifferenceImageViewerWidget = newExpectedActualDifferenceWidget();
         // init Components
         ignoreButton = iconButton(ignoreCurrentIssueAction);
         issuesList = vlist(issuesListModel, l -> {
-            l.setVisibleRowCount(VISIBLE_ISSUES_COUNT);
+            l.setVisibleRowCount(Style.VISIBLE_ISSUES_COUNT);
             l.setCellRenderer(
                     newListCellRenderer(SnapshotIssue.class, SnapshotIssue::getLabel));
         });
         issuesList.setBorder(null);
         labelsForLegend = new JLabel[]{
-                legendLabel(" Expected ", EXPECTED_BORDER_COLOR),//NON-NLS
-                legendLabel(" Actual ", ACTUAL_BORDER_COLOR), //NON-NLS
-                legendLabel(" Difference ", DIFFERENCE_BORDER_COLOR) //NON-NLS)
+                legendLabel(" Expected ", expectedActualDifferenceImageViewerWidget.getExpectedBorderColor()),//NON-NLS
+                legendLabel(" Actual ", expectedActualDifferenceImageViewerWidget.getActualBorderColor()), //NON-NLS
+                legendLabel(" Difference ", expectedActualDifferenceImageViewerWidget.getDifferenceBorderColor()) //NON-NLS)
         };
-        labelsForImages = new JLabel[]{new JLabel(), new JLabel(), new JLabel()};
-        imagesContainer = flowLeft(c -> {
-            c.setOpaque(true);
-            c.setBackground(Color.white);
-            c.setBorder(null);
-        }, labelsForImages);
         imagesLegendContainer = flowLeft(DEFAULT_FLOW_GAP, 0, labelsForLegend);
 
         selectedIssueDescriptionLabel = label();
@@ -156,7 +142,7 @@ class SnapshotReviewPane<T extends SnapshotIssue> extends JPanel {
         variantsIndicator = flowLeft(DEFAULT_FLOW_GAP, 0);
         // make the panel so small only one bullet fits into the row,
         // so the bullets are stacked vertically
-        variantsIndicator.setPreferredSize(new Dimension(BULLET_SIZE, Integer.MAX_VALUE));
+        variantsIndicator.setPreferredSize(new Dimension(Style.BULLET_SIZE, Integer.MAX_VALUE));
         variantsIndicator.setOpaque(true);
         variantsIndicator.setBackground(Color.white);
         layoutComponents();
@@ -176,7 +162,7 @@ class SnapshotReviewPane<T extends SnapshotIssue> extends JPanel {
 
     private static JLabel legendLabel(String title, Color color) {
         JLabel label = new JLabel(title);
-        label.setBorder(createLineBorder(color, LEGEND_BORDER_SIZE));
+        label.setBorder(createLineBorder(color, Style.LEGEND_BORDER_SIZE));
         label.setFont(new Font(Font.SANS_SERIF, Font.ITALIC, 12));
         label.setForeground(Color.GRAY);
         //noinspection StringConcatenation
@@ -197,7 +183,7 @@ class SnapshotReviewPane<T extends SnapshotIssue> extends JPanel {
         JComponent content = bordered()
                 .top(topPart())
                 .left(variantsIndicator)
-                .center(scrollingNoBorder(imagesContainer))
+                .center(scrollingNoBorder(expectedActualDifferenceImageViewerWidget.getComponent()))
                 .bottom(bottomPart());
 
         setLayout(new BorderLayout());
@@ -206,7 +192,6 @@ class SnapshotReviewPane<T extends SnapshotIssue> extends JPanel {
 
     private void initNotifications() {
         issuesList.addListSelectionListener(e -> onSelectedIssueChanged());
-        onComponentResized(imagesContainer, e -> onImagesContainerVisibleRectChanged());
     }
 
     private JComponent topPart() {
@@ -227,7 +212,7 @@ class SnapshotReviewPane<T extends SnapshotIssue> extends JPanel {
     @NonNull
     private BorderedPanel bottomPart() {
         return borderedWithTopLine()
-                .top(bordered(l -> l.setBackground(TITLE_BAR_COLOR))
+                .top(bordered(l -> l.setBackground(Style.TITLE_BAR_COLOR))
                         .left(flowLeft(DEFAULT_FLOW_GAP, 0, label("Issues:"))) //NON-NLS
                         .right(flowLeft(DEFAULT_FLOW_GAP, 0,
                                 iconButton(previousScreenshotAction),
@@ -353,43 +338,30 @@ class SnapshotReviewPane<T extends SnapshotIssue> extends JPanel {
     }
 
     private void rotateImages() {
-        expectedImageIndex = (expectedImageIndex + 1) % 3;
-        onExpectedImageIndexChanged();
+        setExpectedImageIndex((getExpectedImageIndex() + 1) % 3);
     }
 
-    private void updateLabelsForImages() {
-        @Nullable SnapshotImages images = getSnapshotImages();
-        if (images != null) {
-            setIconAndLinedBorder(
-                    labelsForImages[(expectedImageIndex) % 3],
-                    images.getExpectedImage(),
-                    EXPECTED_BORDER_COLOR);
-            setIconAndLinedBorder(
-                    labelsForImages[(expectedImageIndex + 1) % 3],
-                    images.getActualImage(),
-                    ACTUAL_BORDER_COLOR);
-            setIconAndLinedBorder(
-                    labelsForImages[(expectedImageIndex + 2) % 3],
-                    images.getDifferenceImage(),
-                    DIFFERENCE_BORDER_COLOR);
+    private int getExpectedImageIndex() {
+        return expectedActualDifferenceImageViewerWidget.getExpectedImageIndex();
+    }
 
-            onLabelsForImagesChanged();
-        }
+    private void setExpectedImageIndex(int value) {
+        expectedActualDifferenceImageViewerWidget.setExpectedImageIndex(value);
+        onExpectedImageIndexChanged();
     }
 
     private void updateLabelsForLegend() {
         imagesLegendContainer.removeAll();
-        imagesLegendContainer.add(labelsForLegend[(3 - expectedImageIndex) % 3]);
-        imagesLegendContainer.add(labelsForLegend[(4 - expectedImageIndex) % 3]);
-        imagesLegendContainer.add(labelsForLegend[(5 - expectedImageIndex) % 3]);
+        imagesLegendContainer.add(labelsForLegend[(3 - getExpectedImageIndex()) % 3]);
+        imagesLegendContainer.add(labelsForLegend[(4 - getExpectedImageIndex()) % 3]);
+        imagesLegendContainer.add(labelsForLegend[(5 - getExpectedImageIndex()) % 3]);
         imagesLegendContainer.validate();
     }
 
 
     private void onSelectedIssueChanged() {
         invokeLater(() -> {
-            updateLabelsForImages();
-            updateImagesContainer();
+            expectedActualDifferenceImageViewerWidget.setSnapshotIssue(getSelectedIssue());
             updateVariantsIndicator();
             updateSelectedIssueDescriptionLabel();
             ensureSelectionIfPossible();
@@ -399,66 +371,15 @@ class SnapshotReviewPane<T extends SnapshotIssue> extends JPanel {
         });
     }
 
-    private void onImagesContainerVisibleRectChanged() {
-        if (getShrinkToFit()) {
-            invokeLater(this::updateLabelsForImages);
-        }
-    }
-
-    private void onLabelsForImagesChanged() {
-        invokeLater(this::updateImagesContainer);
-    }
 
     private void onExpectedImageIndexChanged() {
-        invokeLater(() -> {
-            updateLabelsForImages();
-            updateLabelsForLegend();
-        });
+        invokeLater(this::updateLabelsForLegend);
     }
 
     private void onShrinkToFitChanged() {
-        invokeLater(() -> {
-            shrinkToFitCheckBox.update();
-            updateLabelsForImages();
-        });
+        invokeLater(shrinkToFitCheckBox::update);
     }
 
-    @Nullable
-    private SnapshotImages getSnapshotImages() {
-        @Nullable SnapshotIssue issue = getSelectedIssue();
-        if (issue == null) {
-            return null;
-        }
-        @Nullable SnapshotImages images = snapshotImages;
-        if (images == null
-                || !Objects.equals(images.getIssue(), issue)
-                || !Objects.equals(getImagesArea(), images.getArea())) {
-            images = new SnapshotImages(issue, getImagesArea());
-            snapshotImages = images;
-        }
-        return images;
-    }
-
-    private void setIconAndLinedBorder(JLabel label, ImageIcon icon, Color borderColor) {
-        label.setIcon(icon);
-        label.setBorder(SwingUtil.lineBorder(borderColor, BORDER_SIZE));
-    }
-
-    @Nullable
-    private Dimension getImagesArea() {
-        if (getShrinkToFit()) {
-            Rectangle visibleRect = imagesContainer.getVisibleRect();
-            int w = visibleRect.width - 4 * SwingUtil.DEFAULT_FLOW_GAP - 6 * BORDER_SIZE;
-            int h = visibleRect.height - 2 * SwingUtil.DEFAULT_FLOW_GAP - 2 * BORDER_SIZE;
-            return new Dimension(max(0, w), max(0, h));
-        } else {
-            return null;
-        }
-    }
-
-    private void updateImagesContainer() {
-        SwingUtil.setVisible(getSelectedIssue() != null, labelsForImages);
-    }
 
     private void updateVariantsIndicator() {
         @Nullable T selectedIssue = getSelectedIssue();
@@ -468,19 +389,23 @@ class SnapshotReviewPane<T extends SnapshotIssue> extends JPanel {
             int sel = getVariantsIndex(selectedIssue);
             for (int i = 0; n > 1 && i < n; i++) {
                 variantsIndicator.add(
-                        label(i == sel ? "●" : "○", c -> c.setFont(BULLET_FONT)));
+                        label(i == sel ? "●" : "○", c -> c.setFont(Style.BULLET_FONT)));
             }
             variantsIndicator.repaint();
             variantsIndicator.revalidate();
         }
     }
 
-    public boolean getShrinkToFit() {
-        return shrinkToFit;
+    private boolean getShrinkToFit() {
+        return expectedActualDifferenceImageViewerWidget.getShrinkToFit();
+    }
+
+    private void setShrinkToFit(boolean value) {
+        expectedActualDifferenceImageViewerWidget.setShrinkToFit(value);
     }
 
     private void toggleShrinkToFit() {
-        shrinkToFit = !shrinkToFit;
+        setShrinkToFit(!getShrinkToFit());
         onShrinkToFitChanged();
     }
 
